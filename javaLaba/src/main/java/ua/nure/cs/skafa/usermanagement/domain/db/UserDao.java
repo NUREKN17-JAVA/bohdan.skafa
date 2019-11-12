351cd04177ab4ca1277e1d231641699e84545372
Bohdan-sk <55053127+Bohdan-sk@users.noreply.github.com>
2019-11-12 09:09:26 +0200

    fix-M

---
+++ javaLaba/src/main/java/ua/nure/cs/skafa/usermanagement/domain/db/UserDao.java
@@ -0,0 +1,20 @@
+package ua.nure.cs.skafa.usermanagement.domain.db;
+
+import java.util.Collection;
+import ua.nure.cs.skafa.usermanagement.domain.User;
+
+public interface UserDao {
+	
+	User create(User user) throws DatabaseException;
+	
+	void update(User user) throws DatabaseException;
+	
+	void delete(User user) throws DatabaseException;
+	
+	User find(Long id) throws DatabaseException;
+	
+	Collection findAll() throws DatabaseException;
+	
+	void setConnectionFactory(ConnectionFactory connectionFactory);
+
+}
