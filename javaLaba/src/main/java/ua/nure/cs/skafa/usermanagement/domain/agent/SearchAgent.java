bbd8c383d10b1c69c740906b7fe49ab2a7de5c1e
Bohdan-sk <bohdan.skafa@nure.ua>
2019-12-24 21:18:22 +0200

    step 2/1

--- javaLaba/src/main/java/ua/nure/cs/skafa/usermanagement/domain/agent/SearchAgent.java
+++ javaLaba/src/main/java/ua/nure/cs/skafa/usermanagement/domain/agent/SearchAgent.java
@@ -41,4 +41,57 @@ public class SearchAgent extends Agent {
             e.printStackTrace();
         }
 
-     
+        addBehaviour(new TickerBehaviour(this, 60000)  {
+            @Override
+            protected void onTick() {
+                DFAgentDescription agentDescription = new DFAgentDescription();
+                ServiceDescription serviceDescription = new ServiceDescription();
+                serviceDescription.setType("searching");
+                agentDescription.addServices(serviceDescription);
+                try {
+                    DFAgentDescription[] descriptions = DFService.search(myAgent, agentDescription);
+                    aids = new AID[descriptions.length];
+                    for (int i = 0; i < descriptions.length; i++) {
+                        DFAgentDescription description1 = descriptions[i];
+                        aids[i] = description1.getName();
+                    }
+                } catch (FIPAException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        addBehaviour(new RequestServer());
+    }
+
+    
+    @Override
+    protected void takeDown() {
+        System.out.println(getAID().getName() + " finished");
+        try {
+            DFService.deregister(this);
+        } catch (FIPAException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void search(String firstName, String lastName) throws SearchException {
+        try {
+            Collection<User> users = DaoFactory.getInstance().getUserDao().findAll(firstName, lastName);
+            if (users.size() > 0) {
+                showUsers(users);
+            } else {
+                addBehaviour(new SearchRequestBehavior(aids, firstName, lastName));
+            }
+        } catch (DatabaseException e) {
+            e.printStackTrace();
+        }
+        gui.setVisible(false);
+        gui.dispose();
+        super.takeDown();
+    }
+
+    void showUsers(Collection<User> users) {
+        gui.addUsers(users);
+    }
+}
--- javaLaba/src/main/java/ua/nure/cs/skafa/usermanagement/domain/db/HsqldbUserDao.java
+++ javaLaba/src/main/java/ua/nure/cs/skafa/usermanagement/domain/db/HsqldbUserDao.java
@@ -191,9 +191,5 @@ class HsqldbUserDao implements UserDao {
 	}
 
 	@Override
-	public Collection findAll(String firstName, String lastName) throws DatabaseException {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
